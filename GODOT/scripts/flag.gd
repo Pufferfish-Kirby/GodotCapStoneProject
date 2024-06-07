47c4176fc3518191aabfc384da7d6b0ad1f9c6a6
Pufferfish-Kirby <68518507+Pufferfish-Kirby@users.noreply.github.com>
2024-06-07 10:57:34 -0400

    Add files via upload
    Project as of June 7th, 2024

---
+++ GODOT/scripts/flag.gd
@@ -0,0 +1,16 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	global_position = global_position
+
+
+
+func _on_area_2d_area_entered(area):
+	pass # Replace with function body.
