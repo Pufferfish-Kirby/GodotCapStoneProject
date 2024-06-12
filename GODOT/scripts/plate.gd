4588ecd7cb48006e0ab788d8be90b53173096841
Pufferfish-Kirby <68518507+Pufferfish-Kirby@users.noreply.github.com>
2024-06-12 19:21:28 -0400

    Game as of June 12th, 2024

---
+++ GODOT/scripts/plate.gd
@@ -0,0 +1,22 @@
+extends StaticBody2D
+
+
+func _ready():
+	set_process(false)
+	
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame .
+func _process(delta):
+	global_position = global_position
+
+
+func _on_area_2d_body_entered(body):
+	if body.name == "egg":
+		set_process(true)
+		print('plate timer')
+		$BreakTimer.start(3)
+
+
+func _on_break_timer_timeout():
+	print('BreakTimer Stopped')
+	queue_free()
